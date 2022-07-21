77a0dcfefd93ac3655afb5c826d69abb3e8801ea
Damian Szerszen <dev.dszerszen@gmail.com>
2022-07-21 20:16:30 +0200

    cleaned up colors & dimens

---
+++ app/src/main/java/pl/dszerszen/bestbefore/ui/theme/Dimens.kt
@@ -0,0 +1,23 @@
+package pl.dszerszen.bestbefore.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimens(
+    val default: Dp = 0.dp,
+    val small: Dp = 4.dp,
+    val medium: Dp = 8.dp,
+    val large: Dp = 16.dp,
+)
+
+val LocalDimens = compositionLocalOf { Dimens() }
+
+val dimens: Dimens
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalDimens.current
+
+
--- app/src/main/java/pl/dszerszen/bestbefore/ui/theme/Theme.kt
+++ app/src/main/java/pl/dszerszen/bestbefore/ui/theme/Theme.kt
@@ -1,30 +1,40 @@
 package pl.dszerszen.bestbefore.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+private val LightColorPalette = darkColors(
+    primary = Color(104, 159, 56, 255),
+    primaryVariant = Color(25, 118, 210, 255),
+    secondary = Color(245, 124, 0, 255),
     background = Color.White,
     surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
+    onPrimary = Color(56, 142, 60, 255),
+    onSecondary = Color(211, 47, 47, 255),
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+    error = Color.Red
+)
+
+private val DarkColorPalette = lightColors(
+    primary = Color(0, 77, 64, 255),
+    primaryVariant = Color(49, 27, 146, 255),
+    secondary = Color(191, 54, 12, 255),
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color(104, 159, 56, 255),
+    onSecondary = Color(255, 179, 0, 255),
+    onBackground = Color.White,
+    onSurface = Color.White,
+    error = Color.Red
 )
 
 @Composable
@@ -34,11 +44,14 @@ fun BestBeforeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
     } else {
         LightColorPalette
     }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalDimens provides Dimens()
+    ) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = { Surface(modifier = Modifier.fillMaxSize(), content = content) }
+        )
+    }
 }
